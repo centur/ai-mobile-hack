ce3a326bd290c323071c729323c451ce8618f9c8
Alexey Shcherbak <alexey@sharpops.com>
2026-08-28 17:39:01 +1000

    WalkieTalkie init

---
+++ WalkieTalkie/WalkieTalkie/WalkieTalkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkieTalkieApp.swift
+//  WalkieTalkie
+//
+//  Created by Alexey Shcherbak on 28/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct WalkieTalkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
